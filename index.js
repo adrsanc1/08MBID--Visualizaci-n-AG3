56a94a1d115da018fafe3cd06dea046d3f818f48
adrsanc1 <130184435+adrsanc1@users.noreply.github.com>
2023-06-21 11:37:26 +0200

    1 file modified

--- index.js
+++ index.js
@@ -16,19 +16,6 @@ var visualization = d3plus.viz()
 .y("value")
 .draw()
 
-//Gráfico de columnas múltiples
-d3.json("https://raw.githubusercontent.com/raul27868/07MBIG-VisualizacionActividades-Guiadas/master/columnas.json", function(data) {
-var visualization = d3plus.viz()
-.container("#viz2")
-.data(data)
-.type('bar')
-.id('name')
-.x('year')
-.y('value')
-.axes({ ticks: 'false' })
-.draw();
-});
-
 var data = [
   {"year": 1991, "name":"alpha", "value": 15},
   {"year": 1991, "name":"beta", "value": 10},
@@ -47,3 +34,31 @@ var data = [
   {"year": 1994, "name":"gamma", "value": 25},
   {"year": 1994, "name":"delta", "value": 32}
 ]
+
+//Gráfico de columnas múltiples
+var visualization = d3plus.viz()
+.container("#viz2")
+.data(data)
+.type('bar')
+.id('name')
+.x('year')
+.y('value')
+.axes({ ticks: 'false' })
+.draw();
+
+
+// Filtrar los datos para los años 1991 y 1992
+var filteredData = data.filter(function(d) {
+  return d.year === 1991 || d.year === 1992;
+});
+
+
+// Configuración del gráfico
+var visualization = d3plus.viz()
+.container("#viz3")
+.data(filteredData)
+.type("box")
+.id("name")
+.x("year")
+.y("value")
+.draw();
